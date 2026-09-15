4a728668808f45f9d9b54120d8b4bbf851a9fc30
byadiganteng-blip <byadiganteng@gmail.com>
2026-09-15 16:48:49 +0700

    fix: import java.io.File di SequencerFragment

--- android/app/src/main/java/com/yad/musicstudio/SequencerFragment.kt
+++ android/app/src/main/java/com/yad/musicstudio/SequencerFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.fragment.app.Fragment
+import java.io.File
 
 class SequencerFragment : Fragment() {
 
